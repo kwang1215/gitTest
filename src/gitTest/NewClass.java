a44e54d06edee380793d2dd1439607e7a024b8e7
Green_Computer <Green_Computer@DESKTOP-912HOCH>
2020-11-10 11:29:57 +0900

    hotfix3

--- src/gitTest/NewClass.java
+++ src/gitTest/NewClass.java
@@ -4,6 +4,8 @@ public class NewClass {
 
 	public static void main(String[] args) {
 		System.out.println("** New Class 추가 **");
+		System.out.println("** hotfix3 추가 **");
+		System.out.println("** hotfix3 제발좀 추가 **");
 	}
 
 }
